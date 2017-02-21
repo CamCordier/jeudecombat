d8861f4cd1b55c0d649d23026dbbb8c169bc5473
sally cohen <saliloucohen18@yahoo.fr>
2017-02-21 10:49:58 +0100

    structure

---
+++ views/index.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<img src="" class="background">
+
+
+<a href="views/choix.php" class="myButton">start</a>
+
+</body>
+
+
+</html>
