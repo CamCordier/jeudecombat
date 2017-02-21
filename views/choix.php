15f911a6e504945ce37ed87ed77fe0833239baab
sally cohen <saliloucohen18@yahoo.fr>
2017-02-21 12:20:37 +0100

    page 1 et choix

---
+++ views/choix.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Choix du personnage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Guerrier</h2>
+            <p>Ce personnage se sert de son incroyable force surhumaine pour attaquer son adversaire. Il est musclé et détruit tout sur son passage. </p>
+            <p><img src="http://s1.1zoom.me/big3/125/342800-sepik.jpg" class="guerrier""><hr>
+            <a class="btn btn-default" href="#" role="button">Choisir ce profil  &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+            <h2>Magicienne</h2>
+            <p>Ce personnage utilise la magie noire, mieux que la magie blanche pour déstabiliser son adversaire. Il jette des sorts. </p>
+            <p><img src="https://s-media-cache-ak0.pinimg.com/originals/3f/e7/8b/3fe78bde62ba6a93cd8be91ea08a16f4.jpg" class="magicienne"><hr>
+            <a class="btn btn-default" href="#" role="button">Choisir ce profil  &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+            <h2>Paladin</h2>
+            <p>Ce personnage réunit la force et la magie blanche. Moins fort que le guerrier dans sa force mais peut utiliser des sorts de magie blanche
+                pour s'en sortir face à l'ennemi.</p>
+            <p><img src="http://s1.1zoom.me/big7/186/World_of_WarCraft_485690.jpg" class="paladin"><hr>
+            <a class="btn btn-default" href="#" role="button">Choisir ce profil &raquo;</a></p>
+        </div>
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+
+</html>
