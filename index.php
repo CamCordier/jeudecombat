6357228fac5fd77bc23051e3f6968cdc4629d179
CamCordier <camille.cordier@devinci.fr>
2017-02-21 11:03:20 +0100

    mise en place du bootstrap et titre de la page de démarrage

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
-<!Doctype html>
-
+<!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8">
-    <title>Titre de la page</title>
+    <title>Page de démarrage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body>
-<img src="" class="background">
+<div class="row">
+    <div class="col-md-6">
+        <img src="" class="background">
+        <a href="views/choix.php" class="myButton">Start</a>
+    </div>
+</div>
 
 
-<a href="views/choix.php" class="myButton">start</a>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 
 </body>
 
