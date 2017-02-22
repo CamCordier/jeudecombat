f7e187fd72dc4872fdf79081d231b1fb8380ba8b
sally cohen <saliloucohen18@yahoo.fr>
2017-02-22 12:08:10 +0100

    GAME OVER page

---
+++ views/gameover.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Choix du personnage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+
+   <body>
+        <div class="portfogame">
+              <div class="textgame">
+                 <p>GAME OVER</p>
+              </div>
+         </div>
+
+    </body>
+
+</html>
