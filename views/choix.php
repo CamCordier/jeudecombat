be5fb3485c42cc3f5848b61bccdc62a2ae425725
CamCordier <camille.cordier@devinci.fr>
2017-02-22 09:48:41 +0100

    Design terminé de la page choix.php

--- views/choix.php
+++ views/choix.php
@@ -18,42 +18,39 @@
 <body>
 <div class="container">
     <div class="choisir">
-         <h1> - Choix de votre personnage - </h1>
+         <h1>Choix de votre personnage</h1>
+
+
+    <figure class="snip0058"><img src="http://s1.1zoom.me/big3/125/342800-sepik.jpg" alt="sample57"/>
+        <figcaption>
+            <p><span>Ce personnage se sert de son incroyable force surhumaine pour attaquer son adversaire.
+                Il est musclé et détruit tout sur son passage.</span></p>
+            <h2 class="white"><span><a href="donjon.php">Guerrier</a></span></h2>
+
+        </figcaption>
+    </figure>
+    <figure class="snip0058"><img src="https://s-media-cache-ak0.pinimg.com/originals/3f/e7/8b/3fe78bde62ba6a93cd8be91ea08a16f4.jpg" alt="sample37"/>
+        <figcaption>
+            <p><span>Ce personnage utilise la magie noire, mieux que la magie blanche pour déstabiliser son adversaire. Il jette des sorts.</span></p>
+            <h2><span><a href="donjon.php">Magicienne</a></span></h2>
+        </figcaption>
+    </figure>
+    <figure class="snip0058"><img src="http://s1.1zoom.me/big7/186/World_of_WarCraft_485690.jpg" alt="sample37" a href="donjon.php"/>
+        <figcaption>
+            <p><span>Ce personnage réunit la force et la magie blanche. Moins fort que le guerrier dans sa force mais peut utiliser des sorts de magie blanche
+                pour s'en sortir face à l'ennemi.</span></p>
+            <h2><span><a href="donjon.php">Paladin</a></span></h2>
+        </figcaption>
+    </figure>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2 style="color: darkblue; margin-bottom: 1em;">Guerrier</h2>
-            <p>Ce personnage se sert de son incroyable force surhumaine pour attaquer son adversaire.
-                Il est musclé et détruit tout sur son passage. </p>
-<br>        <div class="guerrier">
-            <img src="http://s1.1zoom.me/big3/125/342800-sepik.jpg">
-                </div><hr>
-            <p> <a class="btn btn-default" href="guerrier.php" role="button">Choisir ce profil  &raquo;</a></p>
-        </div>
-
-        <div class="col-md-4"><h2 style="color: darkmagenta; margin-bottom: 1em;">Magicienne</h2>
-            <p>Ce personnage utilise la magie noire, mieux que la magie blanche pour déstabiliser son adversaire. Il jette des sorts. </p>
-            <br>
-            <div class="magicienne">
-                <img src="https://s-media-cache-ak0.pinimg.com/originals/3f/e7/8b/3fe78bde62ba6a93cd8be91ea08a16f4.jpg"><hr>
-           </div>
-            <p><a class="btn btn-default" href="magicien.php" role="button">Choisir ce profil  &raquo;</a></p>
-        </div>
-
-        <div class="col-md-4">
-            <h2 style="color: darkred;margin-bottom: 1em;">Paladin</h2>
-            <p>Ce personnage réunit la force et la magie blanche. Moins fort que le guerrier dans sa force mais peut utiliser des sorts de magie blanche
-                pour s'en sortir face à l'ennemi.</p>
-            <br>
-            <div class="paladin">
-                <img src="http://s1.1zoom.me/big7/186/World_of_WarCraft_485690.jpg"><hr>
-            </div>
-                <p> <a class="btn btn-default" href="paladin.php" role="button">Choisir ce profil &raquo;</a></p>
-        </div>
-
-    </div>
-
+<script>  /* Demo purposes only */
+   $("figure").mouseleave(
+       function () {
+          $(this).removeClass("hover");
+       }
+   );</script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
